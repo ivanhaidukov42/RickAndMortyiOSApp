ac59ebf545cfb8e02fad844e81312f638e7fad8f
Ivan Haidukov <chatgpt1488@gmail.com>
2025-09-29 18:25:33 +0300

    Create API request

--- RickAndMorty/IPIClients/RMService.swift
+++ RickAndMorty/IPIClients/RMService.swift
@@ -15,11 +15,15 @@ final class RMService {
     /// Privatized constructor
     private init() {}
     
-    /// Send Rick amd Morty API Call
+    /// Send Rick amd Morty API C all
     /// - Parameters:
     ///   - request: Request instance
+    ///   - type: The type of object we expect to get back
     ///   - completion: Callback with data or error
-    public func execute(_ request: RMRequest, completion: @escaping () -> Void) {
-        
+    public func execute<T: Codable>(
+        _ request: RMRequest,
+        expecting type: T.Type,
+        completion: @escaping (Result<T, Error>) -> Void
+    ) {
     }
 }
